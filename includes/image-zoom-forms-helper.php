a9cc7c75d48cd93575c146748bb2f3fd346afe4a
diana_burduja <diana_burduja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-01 20:13:36 +0000

    tagging version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-image-zoooom/tags/1.0.5@1172155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/image-zoom-forms-helper.php
+++ includes/image-zoom-forms-helper.php
@@ -2,25 +2,25 @@
 
 class ImageZoooom_FormsHelper {
 
-    public static $label_class = 'col-sm-3 control-label';
+    public $label_class = 'col-sm-5 control-label';
 
-    public static function input( $type, $settings = array() ) {
+    public function input( $type, $settings = array() ) {
         $allowed_types = array( 'radio', 'input_text', 'buttons', 'input_color', 'checkbox' );
 
         if ( ! in_array( $type, $allowed_types ) ) {
             return;
         }
-        call_user_func( 'ImageZoooom_FormsHelper::' . $type, $settings );
+        call_user_func( array($this, $type), $settings );
     }
     
-    public static function radio($args = array()) {
+    public function radio($args = array()) {
         if ( !isset($args['label'] )) return;
         if ( !isset($args['name'] )) return;
         if ( !isset($args['values'] ) || count($args['values']) == 0 ) return;
         if ( !isset($args['active'] ) ) $args['active'] = '';
         ?>
            <div class="form-group">
-             <label class="<?= self::$label_class ?>"><?= $args['label'] ?></label>
+             <label class="<?= $this->label_class ?>"><?= $args['label'] ?></label>
        	      <div class="col-lg-8">
                 <?php foreach ($args['values'] as $_id => $_label) : ?>
 				<div class="radio">
@@ -35,14 +35,14 @@ class ImageZoooom_FormsHelper {
         <?php
     }
 
-    public static function input_text( $args = array() ) {
+    public function input_text( $args = array() ) {
         if ( ! isset($args['label'] ) ) return;
         if ( ! isset($args['name'] ) ) return;
         if ( ! isset($args['value'] ) ) $args['value'] = '';
         if ( ! isset($args['description'] ) ) $args['description'] = '';
         ?>
         	<div class="form-group">
-            <label for="<?= $args['name'] ?>" class="<?= self::$label_class ?>"><?= $args['label'] ?> <?= self::tooltip($args['description']) ?></label>
+            <label for="<?= $args['name'] ?>" class="<?= $this->label_class ?>"><?= $args['label'] ?> <?= $this->tooltip($args['description']) ?></label>
             <?php if (isset($args['post_input'])) : ?>
                 <div class="input-group">
             <?php else : ?>
@@ -57,13 +57,13 @@ class ImageZoooom_FormsHelper {
     }
 
 
-    public static function input_color( $args = array() ) {
+    public function input_color( $args = array() ) {
         if ( ! isset($args['label'] ) ) return;
         if ( ! isset($args['name'] ) ) return;
         if ( ! isset($args['value'] ) ) $args['value'] = '';
         ?>
             <div class="form-group">
-            <label for="<?= $args['name'] ?>" class="<?= self::$label_class ?>"><?= $args['label'] ?></label>
+            <label for="<?= $args['name'] ?>" class="<?= $this->label_class ?>"><?= $args['label'] ?></label>
 				<div class="input-group">
                 <input type="color" class="form-control" id="<?= $args['name'] ?>" name="<?= $args['name'] ?>" value="<?= $args['value'] ?>">
                 <span class="input-group-addon" id="color-text-color-hex"><?= $args['value'] ?></span>
@@ -73,13 +73,13 @@ class ImageZoooom_FormsHelper {
         <?php
     }
 
-    public static function checkbox( $args = array() ) {
+    public function checkbox( $args = array() ) {
         if ( ! isset($args['label'] ) ) return;
         if ( ! isset($args['name'] ) ) return;
         if ( ! isset($args['value'] ) ) $args['value'] = false;
         ?>
             <div class="form-group">
-            <label for="<?= $args['name'] ?>" class="<?= self::$label_class ?>"><?= $args['label'] ?></label>
+            <label for="<?= $args['name'] ?>" class="<?= $this->label_class ?>"><?= $args['label'] ?></label>
                   <div class="checkbox">
                     <label>
                     <input type="checkbox" id="<?= $args['name'] ?>" name="<?= $args['name'] ?>" <?= ($args['value'] == true) ? 'checked=""' : '' ?> />
@@ -89,22 +89,22 @@ class ImageZoooom_FormsHelper {
         <?php
     }
 
-    public static function buttons( $args = array() ) {
+    public function buttons( $args = array() ) {
         if ( ! isset($args['label'] ) ) return;
         if ( ! isset($args['name'] ) ) return;
         if ( ! isset($args['values'] ) || count($args['values']) == 0 ) return;
         if ( ! isset($args['value'] ) ) $args['value'] = '';
         ?>
         <div class="form-group">
-        <label for="<?= $args['name'] ?>" class="<?= self::$label_class?>"><?= $args['label'] ?></label>
-        <div class="col-sm-9">
+        <label for="<?= $args['name'] ?>" class="<?= $this->label_class?>"><?= $args['label'] ?></label>
+        <div class="col-sm-7">
           <div class="btn-group btn-group-no-margin" data-toggle="buttons" id="btn-group-style-circle">
             <?php foreach( $args['values'] as $_id => $_value ) : ?>
             <label class="btn btn-default<?= ($args['value'] == $_id) ? ' active' : '' ?> ">
             <input type="radio" name="<?= $args['name'] ?>" id="<?= $_id ?>" value="<?= $_id ?>" <?=  ($args['value'] == $_id) ? 'checked' : '' ?> />
             <div class="icon-in-label ndd-spot-icon icon-style-1">
               <div class="ndd-icon-main-element">
-              <img src="<?= self::assets_url() . $_value[0] ?>"<?php 
+              <img src="<?= $this->assets_url() . $_value[0] ?>"<?php 
                 if ( ! empty($_value[1]) ) {
                     echo ' data-toggle="tooltip" data-placement="top" title="'.$_value[1].'" data-originla-title="' . $_value[1] . '"';
                 }
@@ -119,12 +119,12 @@ class ImageZoooom_FormsHelper {
         <?php
     }
 
-    public static function tooltip( $description = '' ) {
+    public function tooltip( $description = '' ) {
         if ( empty($description) ) return '';
-        return '<img src="'.self::assets_url().'/images/question_mark.svg" data-toggle="tooltip" data-placement="top" title="" data-original-title="'.$description.'" />';
+        return '<img src="'.$this->assets_url().'/images/question_mark.svg" data-toggle="tooltip" data-placement="top" title="" data-original-title="'.$description.'" />';
     }
 
-    public static function assets_url() {
+    public function assets_url() {
        $assets_url = ImageZoooom()->plugins_url() . '/assets'; 
        return $assets_url;
     }
